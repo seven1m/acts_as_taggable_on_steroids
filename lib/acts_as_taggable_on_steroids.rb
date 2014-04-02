0e10461043ad7fb2af09b950b2b95e31d21ac9eb
Sammy Larbi <sam@codeodor.com>
2014-04-02 09:12:11 -0500

    move stuff around so it gets found by bundler

---
+++ lib/acts_as_taggable_on_steroids.rb
@@ -0,0 +1,5 @@
+require File.dirname(__FILE__) + '/acts_as_taggable_on_steroids/tag_list'
+require File.dirname(__FILE__) + '/acts_as_taggable_on_steroids/tagging'
+require File.dirname(__FILE__) + '/acts_as_taggable_on_steroids/tag'
+require File.dirname(__FILE__) + '/acts_as_taggable_on_steroids/tags_helper'
+require File.dirname(__FILE__) + '/acts_as_taggable_on_steroids/acts_as_taggable'
