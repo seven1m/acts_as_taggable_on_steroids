0c675479e79822d2c3b953c1814e65fe5b33cbfa
Sammy Larbi <sam@codeodor.com>
2014-04-15 08:37:32 -0500

    quote table names

--- lib/acts_as_taggable_on_steroids/acts_as_taggable.rb
+++ lib/acts_as_taggable_on_steroids/acts_as_taggable.rb
@@ -136,19 +136,19 @@ module ActiveRecord #:nodoc:
         
         def find_options_for_tag_counts(options = {})
           options = options.dup
-          scope = scope(:find)
+          #relation = scope(:find)
           
           conditions = []
           conditions << send(:sanitize_conditions, options.delete(:conditions)) if options[:conditions]
-          conditions << send(:sanitize_conditions, scope[:conditions]) if scope && scope[:conditions]
-          conditions << "#{Tagging.table_name}.taggable_type = #{quote_value(base_class.name)}"
+          #conditions << send(:sanitize_conditions, scope[:conditions]) if relation && relation[:conditions]
+          conditions << "#{Tagging.quoted_table_name}.taggable_type = #{quote_value(base_class.name)}"
           conditions << type_condition unless descends_from_active_record? 
           conditions.compact!
           conditions = conditions.join(" AND ")
           
-          joins = ["INNER JOIN #{table_name} ON #{table_name}.#{primary_key} = #{Tagging.table_name}.taggable_id"]
+          joins = ["INNER JOIN #{quoted_table_name} ON #{quoted_table_name}.#{primary_key} = #{Tagging.quoted_table_name}.taggable_id"]
           joins << options.delete(:joins) if options[:joins].present?
-          joins << scope[:joins] if scope && scope[:joins].present?
+          #joins << relation[:joins] if relation && relation[:joins].present?
           joins = joins.join(" ")
           
           options = { :conditions => conditions, :joins => joins }.update(options)
